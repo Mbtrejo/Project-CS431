7be85df7836c5af0791c2bff059a626cf77f9115
Marlon <mbtrejo093@gmail.com>
2023-04-26 18:45:16 -0700

    added my insert

---
+++ hw4/insert_Trejo.sql
@@ -0,0 +1,9 @@
+-- Insert data into genre, tracklist, and song entities
+
+INSERT INTO Genre VALUES('Blues','Blues is a music genre that originated in African American communities in the southern United States around the end of the 19th century'),
+('Heavy Metal','Heavy metal is a music genre that emerged in the late 1960s and early 1970s,primarily in the United Kingdom and the United States'),
+('Pop','Pop is a popular music genre that originated in the 1950s and is characterized by a focus on catchy melodies, upbeat rhythms, and simple song structures. Pop music often features elements of other musical styles such as rock, R&B, and electronic music. The term "pop" is short for "popular," indicating that the genre is intended to appeal to a wide audience. Pop music has been commercially successful for many decades, with numerous artists achieving international fame and mainstream success within the genre.');
+
+INSERT INTO Tracklist VALUES('The Fame',242,1,'Just Dance'),('The Fame',216,2,'LoveGame'),('The Fame',208,3,'Paparazzi'),('The Fame',237,4,'Poker Face'),('The Fame',175,5,'Eh,Eh'),('The Fame',172,6,'Beautiful,Dirty,Rich'),('The Fame',222,7,'The Fame'),('The Fame',170,8,'Money Honey'),('The Fame',217,9,'Starstruck');
+
+INSERT INTO Song VALUES('Enter Sandman','Metallica','Metallica'),('Master of Puppets','Metallica','Metallica'),('Just Dance','Lady Gaga','Akon,Lady Gaga,RedOne'),('Shallow','Lady Gaga','Anthony Rossomando,Lady Gaga');
